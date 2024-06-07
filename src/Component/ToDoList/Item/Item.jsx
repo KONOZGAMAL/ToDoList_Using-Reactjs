02beb3caecdcb9cb66fd6add76a627cd398c220a
Your Name <you@example.com>
2024-06-07 23:45:08 +0300

    add to do list to local Storage

--- src/Component/ToDoList/Item/Item.jsx
+++ src/Component/ToDoList/Item/Item.jsx
@@ -1,21 +1,27 @@
-import {React , useState} from 'react'
-import '../Input/Input.css'
+import { React, useState } from "react";
+import "../Input/Input.css";
 export default function Item(props) {
-const [hide,setHide]=useState(true)
-const [hideP,setHideP]=useState(true)
-const hideElement =()=>{
-  setHide(!hide)
-}
-const hidePr =()=>{
-  setHideP(!hideP)
-}
+  const [hide, setHide] = useState(true);
+  const deleteElement = (id) => {
+    props.deleteItem(id);
+  };
+  const hidePr = () => {
+    setHide(!hide);
+  };
   return (
-    <div className={hide?'parent':'hide'}>
-      {props.item.length > 0 ?'':<div className='item'>
-          <p  onClick={hidePr} className={hideP?'':'P'}>{props.item.text}</p>
-          <button className='btn' onClick={hideElement}>Delete</button>
-          </div>
-      }
+    <div className="parent">
+      {props.item.length > 0 ? (
+        ""
+      ) : (
+        <div className="item">
+          <p onClick={() => hidePr()} className={hide ? "" : "line-through"}>
+            {props.item.text}
+          </p>
+          <button className="btn" onClick={() => deleteElement(props.item.id)}>
+            Delete
+          </button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- src/Component/ToDoList/PerantToDo/ToDoList.jsx
+++ src/Component/ToDoList/PerantToDo/ToDoList.jsx
@@ -1,23 +1,36 @@
-import React, { useState } from 'react'
-import Input from '../Input/Input'
-import Item from '../Item/Item'
-
+import React, { useEffect, useState } from "react";
+import Input from "../Input/Input";
+import Item from "../Item/Item";
 export default function ToDoList() {
-  const [items , setItems] = useState([])
-  const addData = (Element)=>{
-    setItems((pram)=>{
-      const arr = [...pram]
-      arr.unshift({text:Element , id:Math.random().toString()})
-      return arr
-    })
-  }
+  const [items, setItems] = useState(() => {
+    const savedItems = localStorage.getItem("item_list");
+    return savedItems ? JSON.parse(savedItems) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("item_list", JSON.stringify(items));
+  }, [items]);
+
+  const addData = (Element) => {
+    setItems((pram) => {
+      const arr = [...pram];
+      arr.unshift({ text: Element, id: Date.now() });
+      return arr;
+    });
+  };
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id !== id);
+    });
+  };
   return (
     <div>
-        <Input addData={addData}/>
-        {items.length > 0 ?
-        items.map((item)=>(<Item item={item}/>
-        )):<p className='show'>No goals found. Maybe add one?</p>
-        }
+      <Input addData={addData} />
+      {items.length !== 0 ? (
+        items.map((item, _id) => <Item item={item} key={_id} deleteItem={deleteItem}/>)
+      ) : (
+        <p className="show">No goals found. Maybe add one?</p>
+      )}
     </div>
-  )
+  );
 }
